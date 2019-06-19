9a5b4ef18a7fd7bc227ae33db1a9e917f710782d
Elwen <Elwen@users.noreply.github.com>
2019-06-19 19:06:13 +0300

    Update show info

--- src/index.js
+++ src/index.js
@@ -23,22 +23,43 @@ function requesApi(method, url) {
     const output = JSON.parse(request.responseText);
     let inner = "";
     output.results.forEach(item => {
-      let itemName = item.name || item.title;
-      let itemPremiereDate = item.first_air_date || item.release_date;
-      if (itemPremiereDate) {
-        itemPremiereDate = new Date(itemPremiereDate);
+      const itemName = item.name || item.title;
+      const itemPopularity = item.popularity
+        ? `<div class="card-img-overlay"><span class="badge badge-success">&#9733; ${
+            item.popularity
+          }</span></div>`
+        : "";
+      const itemPoster = item.poster_path
+        ? `<div class="embed-responsive-item"><img src="http://image.tmdb.org/t/p/w500${
+            item.poster_path
+          }" alt="${itemName}" class="img-fluid"></div>`
+        : '<div class="embed-responsive-item bg-light"></div>';
+      let itemPremiereDate = "";
+      let premiereDate = item.first_air_date || item.release_date;
+      if (premiereDate) {
+        premiereDate = new Date(premiereDate);
+        let premiereDay = (premiereDate.getDate() + 1)
+          .toString()
+          .padStart(2, "0");
+        let premiereMonth = (premiereDate.getMonth() + 1)
+          .toString()
+          .padStart(2, "0");
+        let premiereYaer = premiereDate.getFullYear();
         itemPremiereDate = `
-          <p class="card-text text-secondary">
-            Премьера: ${itemPremiereDate.getDate()}.${itemPremiereDate.getMonth() +
-          1}.${itemPremiereDate.getFullYear()}
+          <p class="card-subtitle text-muted">
+            <small>Премьера: ${premiereDay}.${premiereMonth}.${premiereYaer}</small>
           </p>`;
       }
       inner += `
-        <div class="col-12 col-md-4 col-xl-3 mb-3">
+        <div class="col-12 col-sm-6 col-md-4 col-xl-3 mb-3">
           <div class="card h-100">
+            <div class="embed-responsive embed-responsive-1by1 card-img-top border-bottom">
+            ${itemPoster}
+            </div>
+            ${itemPopularity}
             <div class="card-body">
               <h5 class="card-title">${itemName}</h5>
-             ${itemPremiereDate}
+              ${itemPremiereDate}
             </div>
           </div>
         </div>`;
